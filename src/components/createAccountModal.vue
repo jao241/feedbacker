44dc78395b4fa354a785b302d4a4f7d2a9f8cc99
João Vitor Lima Lago Santos <vorque45@gmail.com>
2022-07-20 15:45:44 -0300

    create a modal for sign in

---
+++ src/components/createAccountModal.vue
@@ -0,0 +1,171 @@
+<template>
+  <button :class="colorClass" @click="modalOpen = true">{{ text }}</button>
+
+  <Teleport to="body">
+    <div class="create-account" v-if="modalOpen">
+      <form action="POST">
+        <div class="modal-header">
+          <h2 class="title">Crie uma conta</h2>
+          <button class="close-icon" @click="closeModal">
+            <img src="../assets/close-icon.svg" alt="icone para cancelar" />
+          </button>
+        </div>
+        <div class="fields">
+          <div class="name-field">
+            <label for="name">Nome</label>
+            <input
+              v-model.lazy="form.name"
+              type="text"
+              name="name"
+              id="name-input"
+              placeholder="Igor Halfeld"
+            />
+          </div>
+          <div class="email-field">
+            <label for="email">E-mail</label>
+            <input
+              v-model.lazy="form.email"
+              type="email"
+              name="email"
+              id="email-input"
+              placeholder="igor@exemplo.com"
+            />
+          </div>
+          <div class="password-field">
+            <label for="password">Senha</label>
+            <input
+              v-model.lazy="form.password"
+              type="password"
+              name="password"
+              id="password-input"
+              placeholder="****"
+            />
+          </div>
+          <button @click.prevent="addNewPerson">Criar conta</button>
+        </div>
+      </form>
+    </div>
+  </Teleport>
+</template>
+
+<script>
+import { ref } from "@vue/reactivity";
+
+export default {
+  setup() {
+    const modalOpen = ref(false);
+    const form = ref({
+      name: "",
+      email: "",
+      password: "",
+    });
+
+    function addNewPerson() {
+      console.log(form.value);
+    }
+
+    function cleanFields() {
+      form.value.name = "";
+      form.value.email = "";
+      form.value.password = "";
+    }
+
+    function closeModal() {
+      cleanFields();
+      modalOpen.value = false;
+    }
+
+    return {
+      form,
+      modalOpen,
+      addNewPerson,
+      closeModal,
+    };
+  },
+  props: ["text", "colorClass"],
+};
+</script>
+
+<style scoped>
+button {
+  background: none;
+  border: none;
+  font-size: 18px;
+}
+
+.color-white {
+  color: white;
+  font-weight: 800;
+  padding-right: 60px;
+}
+
+.color-pink {
+  background-color: white;
+  border-radius: 30px;
+  color: #ef4983;
+  font-weight: 800;
+  padding: 10px 20px;
+}
+
+.create-account {
+  width: 100%;
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background-color: rgba(0, 0, 0, 0.2);
+  position: fixed;
+  top: 0;
+}
+
+.create-account form {
+  background-color: white;
+  border-radius: 2px;
+  padding: 30px;
+  width: 400px;
+  height: 400px;
+}
+
+.modal-header {
+  display: flex;
+  justify-content: space-between;
+  margin-bottom: 40px;
+}
+
+.modal-header button img {
+  width: 15px;
+}
+
+.name-field,
+.email-field,
+.password-field {
+  display: flex;
+  flex-direction: column;
+}
+
+.name-field label,
+.email-field label,
+.password-field label {
+  padding-bottom: 10px;
+}
+
+.name-field input,
+.email-field input,
+.password-field input {
+  background-color: #f9f9f9;
+  border: none;
+  border-radius: 3px;
+  padding: 8px 8px;
+  margin-bottom: 20px;
+  font-size: 20px;
+}
+
+form .fields button {
+  background-color: #ef4983;
+  border-radius: 30px;
+  color: white;
+  font-weight: 800;
+  padding: 10px 20px;
+  margin-top: 15px;
+}
+</style>
